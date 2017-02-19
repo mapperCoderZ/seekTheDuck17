27db35ef8c5886996a3c6c41d3b53e2d69e40973
Gilles <gillespockpock@hotmail.fr>
2017-02-19 16:48:12 +0100

    delete ignored

--- config/vendor.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-require("@angular/platform-browser");
-require("@angular/platform-browser-dynamic");
-require("@angular/core");
-require("@angular/common");
-require("@angular/http");
-require("@angular/router");
-require("rxjs");
-//# sourceMappingURL=vendor.js.map
