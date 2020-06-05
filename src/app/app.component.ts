d19367e51434a0cbb58bbfcd9b6808602ef86976
gilles <gillespockpock@hotmail.fr>
2020-06-05 19:15:09 +0200

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'cm-app-component',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
+  constructor(private router: Router) {}
 
+  ngOnInit() {
+    this.router.navigate([''])
+  }
 }
